8ddc66c946afd556046abdc502df19b4b7cd0f45
朱永光 <heavenwing@msn.com>
2017-10-10 16:48:23 +0800

    Add project files.

---
+++ XamarinDevOpsDemo/XamarinDevOpsDemo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using XamarinDevOpsDemo.ViewModels;
+
+using Xamarin.Forms;
+
+namespace XamarinDevOpsDemo.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+    }
+}
